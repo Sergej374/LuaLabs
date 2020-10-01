12e9ed4cd8f500c11c3b258a080001f73ac14f11
Toddlly <hello@gmail.com>
2020-10-01 17:25:26 +0300

    10/1

--- lab-03/lab-03.lua
+++ lab-03/lab-03.lua
@@ -11,8 +11,33 @@ builder:add_from_file('lab-03.glade')
 
 ui = builder.objects
 
+rads = {ui.radbutA, ui.radbutB, ui.radbutC, ui.radbutD}
+
+function update()
+	x = 0
+	y = 0
+	if ui.boxX.active == true then x = 1 end
+	if ui.boxY.active == true then y = 1 end
+	
+	v = x + y * 2
+	rads[v + 1].active = true
+
+	ui.labelRes.label = v
+end
+
+function ui.boxX:on_clicked(...)
+	update()
+end
+
+function ui.boxY:on_clicked(...)
+	update()
+end
+
+
 ui.wnd.title = 'lab-03-Bezuglyi-474'
 ui.wnd.on_destroy = gtk.main.quit
 ui.wnd:show_all()
+
+update()
 
 gtk.main()
---
+++ lab-04/lab-04.lua
@@ -0,0 +1,42 @@
+#!/bin/lua5.3
+--Bezuglyi 474
+
+lgi = require'lgi'
+
+gtk = lgi.Gtk
+pixbuf = lgi.GdkPixbuf.Pixbuf
+gtk.init()
+
+builder = gtk.Builder()
+builder:add_from_file('lab-04.glade')
+
+ui = builder.objects
+
+function ui.btnAdd:on_clicked(...)
+	name = ui.boxName.text
+	value = tonumber(ui.boxValue.text)
+
+	px = pixbuf.new_from_file('png/blueberry.png')
+
+	i = ui.mdl_items:append()
+	ui.mdl_items[i] = {[1] = name, [2] = value, [3] = px }
+end
+
+rdr_txt = gtk.CellRendererText {}
+rdr_pix = gtk.CellRendererPixbuf {}
+
+col1 = gtk.TreeViewColumn { title = 'Name', {rdr_txt, { text = 1}} }
+col2 = gtk.TreeViewColumn { title = 'Value', {rdr_txt, { text = 2}} }
+col3 = gtk.TreeViewColumn { title = 'Image', {rdr_pix, { pixbuf = 3}} }
+
+ui.list_items:append_column(col1)
+ui.list_items:append_column(col2)
+ui.list_items:append_column(col3)
+
+
+ui.wnd.title = 'lab-04-Bezuglyi-474'
+ui.wnd.on_destroy = gtk.main.quit
+ui.wnd:show_all()
+
+
+gtk.main()
